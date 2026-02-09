8d35c6197e9b64cb4af8391a98d7e9be6161fe7e
John Godoi <john.godoi@gmail.com>
2026-02-09 19:23:04 -0300

    Add unit test for payment validation with insufficient balance in User class

--- main.py
+++ main.py
@@ -234,6 +234,15 @@ class TestUser(unittest.TestCase):
         with self.assertRaises(PaymentException):
             user1.pay(user2, 10.00, "Invalid payment with no credit card")
 
+    def test_payment_validation__not_enough_balance(self):
+        user1 = User(username='valid_username1')
+        user2 = User(username='valid_username2')
+        user1.add_credit_card(credit_card_number='4111111111111111')
+        user1.add_to_balance(5.00)
+
+        with self.assertRaises(PaymentException):
+            user1.pay(user2, 10.00, "Invalid payment with not enough balance")
+
     def test_this_works(self):
         with self.assertRaises(UsernameException):
             raise UsernameException()
